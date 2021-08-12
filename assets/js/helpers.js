9439f060e06e400d4859041a1672afcdccd3a7b6
Neal Maze <nealwmaze@gmail.com>
2021-08-11 18:54:56 -0600

    fin?

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,4 +1,81 @@
 
+function ySc(data, selY) {
+    var ret = d3.scaleLinear()
+        .domain([d3.min(data, d => d[selY]) * 0.9,
+            d3.max(data, d => d[selY]) * 1.1
+        ])
+        .range([hgt, 0]);
+    return ret;
+  };
+  
+
+function setDTool(selX, ret, selY) {
+let lab;
+let u;
+
+if(selX === "poverty") {
+    lab = "Poverty: " 
+    u = "%"
+}
+
+else if(selX === "age") {
+    lab = "Age: "
+    u = " yrs"
+}
+
+else {
+    lab = "Household Income: $"
+    u = ""
+}
+
+if(selY === "healthcare") {
+    yLab = "Lack Healthcare: " 
+    yU = "%"
+}
+
+else if(selY === "smokes") {
+    yLab = "Smoke: "
+    yU = "%"
+}
+
+else {
+    yLab = "Obesity"
+    yU = "%"
+}
+
+var dTip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([80, -80])
+    .html(function(d) {
+        return(`<strong>${d.state}</strong><br>${label}${d[selX]}${unit}<br>${yLabel}${d.healthcare}${yUnit}`);
+    });
+
+chGp.call(dTip);
+
+ret
+    .on("mouseover", function(d) {dTip.show(d, this);
+    })
+    .on("mouseout", function(data){dTip.hide(data);
+    });
+return ret;
+}
+
+function setCircles(ret, nueX, selX, nueY, selY) {
+ret.transition()
+    .duration(1500)
+    .attr("cx", d => nueX(d[selX]))
+    .attr("cy", d => nueY(d[selY]))
+return ret;
+}
+
+function setCircTxt(ret, nueX, selX, nueY, selY) {
+ret.transition()
+    .duration(1500)
+    .attr("x", d => nueX(d[selX]))
+    .attr("y", d => nueY(d[selY]))
+return ret;
+}
+
 
 function xSc(data, selX) {
   var ret = d3.scaleLinear()
@@ -6,15 +83,6 @@ function xSc(data, selX) {
           d3.max(data, d => d[selX]) * 1.1
       ])
       .range([0, wdt]);
-  return ret;
-};
-
-function ySc(data, selY) {
-  var ret = d3.scaleLinear()
-      .domain([d3.min(data, d => d[selY]) * 0.9,
-          d3.max(data, d => d[selY]) * 1.1
-      ])
-      .range([hgt, 0]);
   return ret;
 };
 
@@ -33,72 +101,5 @@ function setYAx(nueY, ret) {
   ret.transition()
       .duration(1500)
       .call(upDown);
-  return ret;
-}
-
-function setCircles(ret, nueX, selX, nueY, selY) {
-  ret.transition()
-      .duration(1500)
-      .attr("cx", d => nueX(d[selX]))
-      .attr("cy", d => nueY(d[selY]))
-  return ret;
-}
-
-function setCircTxt(ret, nueX, selX, nueY, selY) {
-  ret.transition()
-      .duration(1500)
-      .attr("x", d => nueX(d[selX]))
-      .attr("y", d => nueY(d[selY]))
-  return ret;
-}
-
-function setDTool(selX, ret, selY) {
-  let lab;
-  let u;
-
-  if(selX === "poverty") {
-      lab = "Poverty: " 
-      u = "%"
-  }
-
-  else if(selX === "age") {
-      lab = "Age: "
-      u = " yrs"
-  }
-
-  else {
-      lab = "Household Income: $"
-      u = ""
-  }
-
-  if(selY === "healthcare") {
-      yLab = "Lack Healthcare: " 
-      yU = "%"
-  }
-
-  else if(selY === "smokes") {
-      yLab = "Smoke: "
-      yU = "%"
-  }
-  
-  else {
-      yLab = "Obesity"
-      yU = "%"
-  }
-
-  var dTip = d3.tip()
-      .attr("class", "d3-tip")
-      .offset([80, -80])
-      .html(function(d) {
-          return(`<strong>${d.state}</strong><br>${label}${d[selX]}${unit}<br>${yLabel}${d.healthcare}${yUnit}`);
-      });
-  
-  chGp.call(dTip);
-
-  ret
-      .on("mouseover", function(d) {dTip.show(d, this);
-      })
-      .on("mouseout", function(data){dTip.hide(data);
-      });
   return ret;
 }
